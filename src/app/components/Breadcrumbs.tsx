37f518f4a63b0cdd1cd6b6e31c3662e933c80674
Zehan Khan <zkhan@hootboard.com>
2023-10-11 01:56:31 +0530

    feat: :sparkles: improvise breadcrumb component

--- src/app/components/Breadcrumbs.tsx
+++ src/app/components/Breadcrumbs.tsx
@@ -1,6 +1,8 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { BiChevronRight } from "react-icons/bi";
+import { AiFillHome } from "react-icons/ai";
 
 const Breadcrumbs = () => {
     const path = usePathname();
@@ -21,19 +23,26 @@ const Breadcrumbs = () => {
 
     const routes = get_breadcrumbs(path);
     return (
-        <div className='flex gap-3'>
-            {
-                routes.map((path, index) => (
-                    <Link
-                        key={path.name}
-                        href={path.name === "Home" ? "/" : "/"+path.name.toLowerCase()}
-                        className={`text-sm ${routes.length - 1 !== index ? "text-gray-400" :"text-black" }`}
-                    >
-                        {path.name + `${routes.length - 1 !== index ? " / " : ""}`}
-                    </Link>)
-                )
-            }
-        </div>
+        <nav className="flex" aria-label="Breadcrumb">
+            <ol role="list" className="flex items-center space-x-4">
+                {routes.map((path, index) => (
+                    <li key={path.name}>
+                        <div className="flex items-center">
+                            {
+                                path.name === "Home" ? "" :
+                                    <BiChevronRight className="h-5 w-5 flex-shrink-0 text-gray-400" />
+                            }
+                            <Link
+                                href={path.name === "Home" ? "/" : "/" + path.name.toLowerCase()}
+                                className={`ml-4 text-sm font-medium hover:text-gray-700 ${routes.length - 1 !== index ? "text-gray-400" : "text-black"}`}
+                            >
+                                {path.name === "Home" ? <AiFillHome className="h-5 w-5 flex-shrink-0" /> : path.name.toLowerCase()}
+                            </Link>
+                        </div>
+                    </li>
+                ))}
+            </ol>
+        </nav>
     )
 }
 
